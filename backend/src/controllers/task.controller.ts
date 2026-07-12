3621ebdca9e1d4ed7ae7ac8b7082954cf22761fc
rk9698020@gmail.com <rk9698020@gmail.com>
2026-07-12 12:23:49 +0530

    Added create task features in backend

---
+++ backend/src/controllers/task.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../middlewares/asyncHandler.middleware";
+import { projectIdSchema } from "../validation/project.validation";
+import { workspaceIdSchema } from "../validation/workspace.validation";
+import { getMemeberRoleWorkspace } from "../services/member.service";
+import { roleGuard } from "../utils/roleGuard";
+import { Permissions } from "../enums/role.enum";
+import { createTaskSchema } from "../validation/task.validation";
+import { createTaskService } from "../services/task.service";
+import { HTTPSTATUS } from "../config/http.config";
+
+export const createTaskController = asyncHandler(
+    async(req : Request, res : Response) => {
+
+        const userId = req.user?._id;
+
+        const body = createTaskSchema.parse(req.body);
+        const projectId = projectIdSchema.parse(req.params.projectId);
+        const workspaceId = workspaceIdSchema.parse(req.params.workspaceId);
+
+        const { role } = await getMemeberRoleWorkspace(userId, workspaceId);
+        roleGuard(role, [Permissions.CREATE_PROJECT]);
+
+        const { task } = await createTaskService(
+            workspaceId,
+            projectId,
+            userId,
+            body
+        );
+
+        return res.status(HTTPSTATUS.OK).json({
+            message : "Task created successfully",
+            task,
+        });
+    }
+);
---
+++ backend/src/routes/task.route.ts
@@ -0,0 +1,10 @@
+
+import { Router } from "express";
+import { createTaskController } from "../controllers/task.controller";
+
+
+const taskRoutes = Router();
+
+taskRoutes.post("/projects/:projectId/workspace/:workspaceId/create", createTaskController);
+
+export default taskRoutes;
