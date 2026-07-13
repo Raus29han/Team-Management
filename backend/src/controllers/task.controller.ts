cc953c9d648a79e7ca4084047e92082e50a74a56
rk9698020@gmail.com <rk9698020@gmail.com>
2026-07-13 12:17:53 +0530

    Added update task features in backend

--- backend/src/controllers/task.controller.ts
+++ backend/src/controllers/task.controller.ts
@@ -5,8 +5,8 @@ import { workspaceIdSchema } from "../validation/workspace.validation";
 import { getMemeberRoleWorkspace } from "../services/member.service";
 import { roleGuard } from "../utils/roleGuard";
 import { Permissions } from "../enums/role.enum";
-import { createTaskSchema } from "../validation/task.validation";
-import { createTaskService } from "../services/task.service";
+import { createTaskSchema, taskIdSchema, updateTaskSchema } from "../validation/task.validation";
+import { createTaskService, updateTaskService } from "../services/task.service";
 import { HTTPSTATUS } from "../config/http.config";
 
 export const createTaskController = asyncHandler(
@@ -19,7 +19,7 @@ export const createTaskController = asyncHandler(
         const workspaceId = workspaceIdSchema.parse(req.params.workspaceId);
 
         const { role } = await getMemeberRoleWorkspace(userId, workspaceId);
-        roleGuard(role, [Permissions.CREATE_PROJECT]);
+        roleGuard(role, [Permissions.CREATE_TASK]);
 
         const { task } = await createTaskService(
             workspaceId,
@@ -32,5 +32,26 @@ export const createTaskController = asyncHandler(
             message : "Task created successfully",
             task,
         });
+    }
+);
+
+export const updateTaskController = asyncHandler(
+    async(req : Request, res : Response) => {
+        const userId = req.user?._id;
+
+        const body = updateTaskSchema.parse(req.body);
+        const taskId = taskIdSchema.parse(req.params.id);
+        const projectId = projectIdSchema.parse(req.params.projectId);
+        const workspaceId = workspaceIdSchema.parse(req.params.workspaceId);
+
+        const { role } = await getMemeberRoleWorkspace(userId, workspaceId);
+        roleGuard(role, [Permissions.EDIT_TASK]);
+
+        const { updatedTask } = await updateTaskService(workspaceId, projectId, taskId, body);
+
+        return res.status(HTTPSTATUS.OK).json({
+            message : "Task updated successfully",
+            task : updatedTask,
+        })
     }
 );
--- backend/src/services/task.service.ts
+++ backend/src/services/task.service.ts
@@ -3,7 +3,7 @@ import MemberModel from "../models/member.model";
 import ProjectModel from "../models/project.model";
 import TaskModel from "../models/task.model";
 import WorkspaceModel from "../models/workspace.model";
-import { NotFoundException } from "../utils/appError";
+import { BadRequestException, NotFoundException } from "../utils/appError";
 
 
 export const createTaskService = async(
@@ -57,6 +57,56 @@ export const createTaskService = async(
 
     return {
         task
+    }
+
+};
+
+export const updateTaskService = async(
+    workspaceId : string,
+    projectId : string,
+    taskId : string,
+    body : {
+        title : string;
+        description? : string;
+        priority : string;
+        status : string;
+        assignedTo? : string | null;
+        dueDate? : string;
+    }
+) => {
+
+    const project = await ProjectModel.findById(projectId);
+
+    if(!project || project.workspace.toString() != workspaceId.toString()) {
+        throw new NotFoundException(
+            "Project not found or does not belong to this workspace"
+        )
+    }
+
+    const task = await TaskModel.findById(taskId);
+
+    if(!task || task.project.toString() != projectId.toString()) {
+        throw new NotFoundException(
+            "Task not found or does not belong to this project"
+        )
+    }
+
+    const updatedTask = await TaskModel.findByIdAndUpdate(
+        taskId,
+        {
+            ...body,
+        },
+        {
+            new : true
+        }
+    );
+
+    if(!updatedTask){
+        throw new BadRequestException("Failed to update task");
+    }
+
+    return {
+        updatedTask,
     }
 
 };
